8830e1e50e9f8539b0f47cb3a471c5f635b1121c
Masoumeh Rezai <you@example.com>
2023-05-18 22:49:22 +0430

    work on event and odd numbers

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,17 @@
-// input content 
-function input() {
-    alert("heoo")
+function give() {
+    const input = document.getElementById("input").value;
+    if (input % 2 == 0) {
+        document.getElementById("div").style.backgroundColor = "green";
+
+    }
+    else if (input % 2 == 1) {
+        document.getElementById("div").style.backgroundColor = "red";
+        const show = () => {
+            document.getElementById("div").style.display = "none";
+
+        }
+        setInterval(show, 1000)
+    }
+
 }
-input()
+give()
