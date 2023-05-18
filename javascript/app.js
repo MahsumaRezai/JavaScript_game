66b5a1f4fca8661f310a3a9d884b937aece21418
Masoumeh Rezai <you@example.com>
2023-05-18 23:04:11 +0430

    work on js file

--- javascript/app.js
+++ javascript/app.js
@@ -1,13 +1,31 @@
 function give() {
     const input = document.getElementById("input").value;
     if (input % 2 == 0) {
-        document.getElementById("div").style.backgroundColor = "green";
+        document.getElementById("div1").style.backgroundColor = "green";
+        document.getElementById("div2").style.backgroundColor = "green";
+        document.getElementById("div3").style.backgroundColor = "green";
+        document.getElementById("div4").style.backgroundColor = "green";
+        document.getElementById("div5").style.backgroundColor = "green";
+        document.getElementById("div6").style.backgroundColor = "green";
+
 
     }
     else if (input % 2 == 1) {
-        document.getElementById("div").style.backgroundColor = "red";
+        document.getElementById("div1").style.backgroundColor = "red";
+        document.getElementById("div2").style.backgroundColor = "red";
+        document.getElementById("div3").style.backgroundColor = "red";
+        document.getElementById("div4").style.backgroundColor = "red";
+        document.getElementById("div5").style.backgroundColor = "red";
+        document.getElementById("div6").style.backgroundColor = "red";
+
         const show = () => {
-            document.getElementById("div").style.display = "none";
+            document.getElementById("div1").style.display = "none";
+            document.getElementById("div2").style.display = "none";
+            document.getElementById("div3").style.display = "none";
+            document.getElementById("div4").style.display = "none";
+            document.getElementById("div5").style.display = "none";
+            document.getElementById("div6").style.display = "none";
+
 
         }
         setInterval(show, 1000)
