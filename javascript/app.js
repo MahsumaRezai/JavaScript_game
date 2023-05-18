3d1f69bfec82a55728bb68e095187911f2b33dd2
Masoumeh Rezai <you@example.com>
2023-05-18 23:29:48 +0430

    work on styles section

--- javascript/app.js
+++ javascript/app.js
@@ -1,16 +1,20 @@
 function give() {
     const input = document.getElementById("input").value;
-    if (input % 2 == 0) {
+    if (input != undefined && input % 2 == 0) {
         document.getElementById("div1").style.backgroundColor = "green";
+        document.getElementById("div1").innerText = "Thanks";
         document.getElementById("div2").style.backgroundColor = "green";
+        document.getElementById("div2").innerText = "You";
         document.getElementById("div3").style.backgroundColor = "green";
+        document.getElementById("div3").innerText = "A lot";
         document.getElementById("div4").style.backgroundColor = "green";
+        document.getElementById("div4").innerText = "For";
         document.getElementById("div5").style.backgroundColor = "green";
+        document.getElementById("div5").innerText = "This";
         document.getElementById("div6").style.backgroundColor = "green";
-
-
+        document.getElementById("div6").innerText = "Game";
     }
-    else if (input % 2 == 1) {
+    else if (input != undefined && input % 2 == 1) {
         document.getElementById("div1").style.backgroundColor = "red";
         document.getElementById("div2").style.backgroundColor = "red";
         document.getElementById("div3").style.backgroundColor = "red";
